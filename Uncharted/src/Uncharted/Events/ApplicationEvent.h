0b8dfdcdb808693bc7473d015af621ea1cc7d374
Horia Jurcut <horia.dragos@gmail.com>
2019-08-06 21:38:44 +0200

    Event System: Add Window events

--- Uncharted/src/Uncharted/Events/ApplicationEvent.h
+++ Uncharted/src/Uncharted/Events/ApplicationEvent.h
@@ -2,32 +2,73 @@
 
 #include "Event.h"
 
+#include <sstream>
+
 namespace Uncharted
 {
 
 	class UNCHARTED_API WindowResizeEvent : public Event
 	{
+	public:
+		WindowResizeEvent(unsigned int width, unsigned int height)
+			: m_Width(width), m_Height(height) {}
 
+		inline unsigned int GetWidth() const
+		{
+			return m_Width;
+		}
+
+		inline unsigned int Getheight() const
+		{
+			return m_Height;
+		}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "WindowResizeEvent: (" << m_Width << ", " << m_Height << ")";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(WindowResize)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	private:
+		unsigned int m_Width;
+		unsigned int m_Height;
 	};
 
 	class UNCHARTED_API WindowCloseEvent : public Event
 	{
+	public:
+		WindowCloseEvent() {}
 
+		EVENT_CLASS_TYPE(WindowClose)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
+
 
 	class UNCHARTED_API AppTickEvent : public Event
 	{
+		AppTickEvent() {}
 
+		EVENT_CLASS_TYPE(AppTick)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
 
 	class UNCHARTED_API AppUpdateEvent : public Event
 	{
+		AppUpdateEvent() {}
 
+		EVENT_CLASS_TYPE(AppUpdate)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
 
 	class UNCHARTED_API AppRenderEvent : public Event
 	{
-
+		AppRenderEvent() {}
+		
+		EVENT_CLASS_TYPE(AppRender)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
 
 }
--- Uncharted/src/Uncharted/Events/MouseEvent.h
+++ Uncharted/src/Uncharted/Events/MouseEvent.h
@@ -1,1 +1,59 @@
 #pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Uncharted
+{
+
+	class UNCHARTED_API MouseMovedEvent : public Event
+	{
+	public:
+		MouseMovedEvent(float x, float y)
+			: m_MouseX(x), m_MouseY(y) {}
+
+		inline float GetX() const
+		{
+			return m_MouseX;
+		}
+
+		inline float GetY() const
+		{
+			return m_MouseY;
+		}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseMovedEvent: (" << m_MouseX << ", " << m_MouseY << ")";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseMoved)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+	private:
+		float m_MouseX;
+		float m_MouseY;
+	};
+
+	class UNCHARTED_API MouseScrolledEvent : public Event
+	{
+
+	};
+
+	class UNCHARTED_API MouseButtonEvent : public Event
+	{
+
+	};
+
+	class UNCHARTED_API MousePressedButtonEvent : public MouseButtonEvent
+	{
+
+	};
+
+	class UNCHARTED_API MouseReleasedButtonEvent : public MouseButtonEvent
+	{
+
+	};
+}
